c31ed024b0c2b53b8499b1afe67b34fd8e33ad52
AcedYu <alexyu299@hotmail.com>
2021-04-04 21:42:05 -0700

    weather function now creates an embed

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,10 @@
-// Load up the token from our environment variable
+// require from node
 require('dotenv').config();
 const fetch = require("node-fetch");
+const Discord = require('discord.js');
 
-const { Client } = require('discord.js');
-const client = new Client();
+// define client
+const client = new Discord.Client();
 // Command prefix will be +
 const PREFIX = "_";
 
@@ -11,8 +12,10 @@ const PREFIX = "_";
 const commandList = {
   hello: (message) => cmdHello(message),
   kevenny: (message) => cmdKevenny(message),
+  knee: (message) => cmdKevenny(message),
   retard: (message) => cmdRetard(message),
-  weather: (message, city) => cmdWeather(message, ...city)
+  weather: (message, city) => cmdWeather(message, ...city),
+  say: (message, args) => cmdSay(message, ...args)
 };
 
 // Ready message on start
@@ -42,7 +45,7 @@ client.on('message', (message) => {
 });
 
 // Command functions
-var cmdHello = (message) => message.channel.send("Hello there, I am Phones's bot. Nice to meet you.");
+var cmdHello = (message) => message.channel.send("Hello there, I am Phones's Bot. Nice to meet you.");
 var cmdKevenny = (message) => message.channel.send("Who? :smirk:");
 var cmdRetard = (message) => message.channel.send("I don't have a Kevenny friendly command like that :rage:");
 
@@ -56,14 +59,27 @@ var cmdWeather = (message, ...city) => {
     .then(function (response) {
       if (response.ok) {
         response.json().then((data) => {
-          // Send Message
-          message.channel.send(
-          `Here is the current weather in ${data.name} :\nTemperature: ${data.main.temp} °F\nHumidity: ${data.main.humidity}%\nWind Speed: ${data.wind.speed} MPH`);
+          // launch renderWeatherEmbed
+          renderWeatherEmbed(message, data);
         });
       } else {
         message.channel.send("Invalid city for Weather command.");
       }
     });
+}
+
+// function renderWeatherEmbed
+var renderWeatherEmbed = (message, data) => {
+  var newEmbed = new Discord.MessageEmbed()
+  .setTitle(`Here is the current weather in ${data.name}`)
+  .setImage(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+  .setDescription(`Temperature: ${data.main.temp} °F\nHumidity: ${data.main.humidity}%\nWind Speed: ${data.wind.speed} MPH`);
+
+  message.channel.send(newEmbed);
+}
+
+var cmdSay = (message) => {
+  message.channel.send("My voice hasn't been implemented yet, will be added in a future patch. :speaking_head:");
 }
 
 // Bot invoke log in
